0e316eaf8d5f0660a13475053526cbcff39caba9
VladSasnyk <sasnykvlad@gmail.com>
2024-02-27 16:13:03 +0200

    upd

--- src/components/Teachers/InfoSection.jsx
+++ src/components/Teachers/InfoSection.jsx
@@ -11,7 +11,7 @@ const InfoSection = ({ experience, students, level, about, changeHandler, name }
     <div className='flex flex-col items-center gap-4 shadow-lg text-center text-lg p-4 max-md:pb-12 max-sm:text-base' id="infoSection">
 
       <h2 className='w-full bg-[#3F7B5B] text-white text-4xl text-center p-2 max-sm:text-2xl'>{name}</h2>
-      {experience && (<p> <span className='font-bold text-lg'>Досвід:</span> {experience},</p>)}
+      {experience && (<p> <span className='font-bold text-lg'>Досвід:</span> {experience}</p>)}
       {students && <p><span className='font-bold text-lg'>Учні:</span> {students} </p>}
       {level && <p><span className='font-bold text-lg'>Рівень англійської:</span> {level}</p>}
       {about && <p><span className='font-bold text-lg'>Про мене:</span> {about}</p>}
